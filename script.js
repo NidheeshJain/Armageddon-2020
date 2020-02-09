7c7218e208b3ac9e073bf1113c7377407a26984f
iagrg2001 <igarg2001@gmail.com>
2020-02-09 22:52:55 +0530

    animated home

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var home = document.querySelector('#home');
+//let vh = window.innerHeight/100;
+//let vw = window.innerWidth/100;
 // //console.log(home)
 // let i = true;
 // console.log(i);
@@ -20,11 +22,13 @@ window.onscroll = function(e) {
     let cenimg = document.querySelector('#center-img');
     let leftimg = document.querySelector('#left-img');
     let rightimg = document.querySelector('#right-img');
+    let arma = document.querySelector('#logo');
     if(scrolldown <= 150) {
         cenimg.style.height = "87vh";
-        cenimg.style.transform = `scale(${1- (scrolldown*0.0008133)}, ${1 - (scrolldown*0.0008133)})`;
-        //zrightimg.style.transform = `translateX(0px)`;
-        //leftimg.style.transform = `translate(-0px)`;
+        cenimg.style.transform = `scale(${1- (scrolldown*0.00085)}, ${1 - (scrolldown*0.00085)})`;
+        rightimg.style.transform = `translate(${(3.5/200 * scrolldown) - 8.0}vw)`;
+        leftimg.style.transform = `translate(${(-3.5/200 * scrolldown) + 13.0}vw)`;
+        arma.style.bottom = `${70 + 0.4 * scrolldown}`;
         cenimg.style.zIndex = '1';
     }
 }
